326e7af9f61847288474014cd5d5e4c3e0c266e6
Damien Clark <damo.clarky@gmail.com>
2016-07-05 09:31:58 +1000

    Added further Moodle filters
    . Started adding filters for OU Blog module
    . Added filters for alt and title tags
    . Added try/catch for debugging

--- Redact.user.js
+++ Redact.user.js
@@ -9,7 +9,7 @@
 // @include     https://handbook.cqu.edu.au/*
 // @grant       GM_addStyle
 // @run-at      document-start
-// @version     1.0.1
+// @version     1.0.2
 // ==/UserScript==
 
 try
@@ -20,57 +20,79 @@ try
 
   var func = function()
   {
-    var redact_text = 
-    [
-      '.useridnumber, .useremail, .idnumber, .email, .subfield_firstname, .subfield_idnumber, .subfield_email',
-      'a[href*="/user/view.php"]',
-      'a[href*="/assignsubmission_file/"]',
-      'td[id^="mod-quiz-report-overview-report"].cell.c3',
-      'td[id^="mod-quiz-report-overview-report"].cell.c4',
-      'div.choicegroups-membersnames',
-      'table[id=completion-progress] tbody tr th a',
-      'select[name=userid] option',
-      'optgroup[label^="Student"] option, optgroup[label^="No roles"] option',
-      'table#completion-progress tbody tr td:nth-child(2), table#completion-progress tbody tr td:nth-child(3)',
-      'table#participants tbody tr td:nth-child(4), table#participants tbody tr td:nth-child(5)',
-      'span.ui-id-1',
-      'table.studentInfo tbody tr:nth-child(3) td',
-      'table.studentAccess tbody tr td:nth-child(1), table.studentAccess tbody tr td:nth-child(2), table.studentAccess tbody tr td:nth-child(3)',
-      //EASI Integration
-      'span[id=ui-id-1].ui-dialog-title',
-      //EASI System
-      'a[href*="/students/studentDetail/"]',
-      'div#nudgeLog h3',
-      'input#emailTo',
-      //Handbook System
-      'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(1) th', /*student name*/
-      'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(2) td:nth-child(2)', /*student number*/
-      'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(3) td:nth-child(2)', /*student name*/
-      'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(3) td:nth-child(2) small span', /*student name*/
-      'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(5) td:nth-child(2) small a', /*email*/
-      'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(6) td:nth-child(2)', /*phone*/
-      'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(7) td:nth-child(2)', /*phone*/
-      'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(8) td:nth-child(2)', /*phone*/
-      'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(9) td:nth-child(2)', /*address*/
-      'a[href^="mailto:"]'
-    ] ;
+    try
+    {
+      var redact_text = 
+      [
+        '.useridnumber, .useremail, .idnumber, .email, .subfield_firstname, .subfield_idnumber, .subfield_email',
+        'a[href*="/user/view.php"]',
+        'a[href*="/assignsubmission_file/"]',
+        'td[id^="mod-quiz-report-overview-report"].cell.c3',
+        'td[id^="mod-quiz-report-overview-report"].cell.c4',
+        'div.choicegroups-membersnames',
+        'table[id=completion-progress] tbody tr th a',
+        'select[name=userid] option',
+        'optgroup[label^="Student"] option, optgroup[label^="No roles"] option',
+        'table#completion-progress tbody tr td:nth-child(2), table#completion-progress tbody tr td:nth-child(3)',
+        'table#participants tbody tr td:nth-child(4), table#participants tbody tr td:nth-child(5)',
+        'span.ui-id-1',
+        'table.studentInfo tbody tr:nth-child(3) td',
+        'table.studentAccess tbody tr td:nth-child(1), table.studentAccess tbody tr td:nth-child(2), table.studentAccess tbody tr td:nth-child(3)',
+        //OU Blog
+        'div.oublog-post-editsummary',
+        //EASI Integration
+        'span[id=ui-id-1].ui-dialog-title',
+        //EASI System
+        'a[href*="/students/studentDetail/"]',
+        'div#nudgeLog h3',
+        'input#emailTo',
+        //Handbook System
+        'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(1) th', /*student name*/
+        'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(2) td:nth-child(2)', /*student number*/
+        'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(3) td:nth-child(2)', /*student name*/
+        'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(3) td:nth-child(2) small span', /*student name*/
+        'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(5) td:nth-child(2) small a', /*email*/
+        'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(6) td:nth-child(2)', /*phone*/
+        'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(7) td:nth-child(2)', /*phone*/
+        'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(8) td:nth-child(2)', /*phone*/
+        'div#studentResultsArea div.tab-content div#cqucentral div#studentResultsArea table tbody tr:nth-child(9) td:nth-child(2)', /*address*/
+        'a[href^="mailto:"]'
+      ] ;
 
-    var redact_image =
-    [
-      '.userpicture',
-      'img[src*="studentphoto.cqu.edu.au"]' /*handbook photo*/
-    ] ;
+      var redact_image =
+      [
+        '.userpicture',
+        'img[src*="studentphoto.cqu.edu.au"]' /*handbook photo*/
+      ] ;
 
-    console.log("Redacting text") ;
-    GM_addStyle(redact_text.join()+' {color: transparent !important; text-shadow: rgba(0, 0, 0, 0.5) 0px 0px 6px !important;}') ;
-    console.log("Text Redacted") ;
+      console.log("Redacting text") ;
+      GM_addStyle(redact_text.join()+' {color: transparent !important; text-shadow: rgba(0, 0, 0, 0.5) 0px 0px 6px !important;}') ;
+      console.log("Text Redacted") ;
 
-    console.log("Redacting Images") ;
-    GM_addStyle(redact_image.join()+' {filter: blur(10px) !important;}') ;
-    console.log("Images Redacted") ;
+      console.log("Redacting Images") ;
+      GM_addStyle(redact_image.join()+' {filter: blur(10px) !important;}') ;
+      console.log("Images Redacted") ;
 
-    console.log("Setting page visible") ;
-    window.document.getElementsByTagName("body")[0].style.visibility = "visible" ;
+      console.log("Removing title and alt attributes") ;
+      redact_text.concat(redact_image).forEach(function(s)
+      {
+				var nodes = document.querySelectorAll(s) ;
+				for(var i=0; i<nodes.length; i++)
+				{
+					nodes[i].removeAttribute('title') ;
+					nodes[i].removeAttribute('alt') ;
+				}
+      }) ;
+
+      console.log("Setting page visible") ;
+      window.document.getElementsByTagName("body")[0].style.visibility = "visible" ;
+    }
+    catch(err)
+    {
+      console.log('Error: '+err) ;
+      console.log('On line: '+err.lineNumber) ;
+      console.log('Stack:'+err.stack) ;
+    }
   } ;
 
   addEventListener('DOMContentLoaded', func, false);
